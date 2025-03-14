82a902c0b40f42a7763e8982e9c647beee8d738b
SpaceyTurtle924 <cciahector@gmail.com>
2025-03-14 08:24:07 -0600

    Update

--- src/app/page.js
+++ src/app/page.js
@@ -89,11 +89,11 @@ export default function Home() {
                   height="24"
                   viewBox="0 0 24 24"
                   fill="none"
-                  stroke="currentColor"
+                  stroke="currentColor" 
                   strokeWidth="2"
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  className="lucide lucide-mail"
+                  className="lucide lucide-mail" 
                 >
                   <rect width="20" height="16" x="2" y="4" rx="2"></rect>
                   <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
